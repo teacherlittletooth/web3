e1f40bc787f67df78a5b0c1ac8630e0c6e64cd77
teacherlittletooth <94143910+teacherlittletooth@users.noreply.github.com>
2023-04-05 09:24:08 -0300

    Create Aula2.java

---
+++ Aula 2/Aula2.java
@@ -0,0 +1,47 @@
+package aula2;
+
+import java.util.Scanner;
+
+public class Aula2 {
+    
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+        try {
+            System.out.println("Qual o seu nome?");
+            String nome = scan.next();
+
+            System.out.println("Qual a sua idade?");
+            int idade = scan.nextInt();
+
+            System.out.println("Qual é o seu peso?");
+            float peso = scan.nextFloat();
+
+            System.out.println("Qual é a sua altura?");
+            float altura = scan.nextFloat();
+
+            float resultado = peso / (altura * altura);
+            String aviso;
+
+            if(resultado < 18.5) {
+                aviso = "Abaixo do peso";
+            } else if(resultado >= 18.5 && resultado < 25) {
+                aviso = "Peso normal";
+            } else if(resultado >= 25 && resultado < 30) {
+                aviso = "Sobrepeso";
+            } else {
+                aviso = "Obesidade";
+            }
+
+            System.out.println("Nome: " + nome + 
+                    "\nIdade: " + idade + " anos." +
+                    "\nPeso: " + peso + " kg" +
+                    "\nAltura: " + altura + " m" +
+                    "\nResultado: " + resultado + " kg/m²" +
+                    "\nAviso: " + aviso);
+        } catch(Exception erro) {
+            System.out.println("Xiiii...deu ruim :/");
+        }
+    }
+    
+}
