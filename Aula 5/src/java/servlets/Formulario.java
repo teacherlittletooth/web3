3d2dc7c9f3b91493c8dfac1230ae9a7ef317996c
teacherlittletooth <94143910+teacherlittletooth@users.noreply.github.com>
2023-04-17 12:21:30 -0300

    Update Formulario.java

--- Aula 5/src/java/servlets/Formulario.java
+++ Aula 5/src/java/servlets/Formulario.java
@@ -2,57 +2,64 @@ package servlets;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 
-@WebServlet(name = "Formulario", urlPatterns = {"/Formulario"})
+@WebServlet(name = "Form", urlPatterns = {"/Form"})
 public class Formulario extends HttpServlet {
 
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         
-        //Capturar dados do formulário, verificando se porventura estão vazios ou nulos
         String data;
-        if(!request.getParameter("data").isEmpty()) {
-             data = request.getParameter("data");
+        if(request.getParameter("data").isEmpty()) {
+            data = "Nenhuma data informada";
         } else {
-            data = "Data não enviada";
+            data = request.getParameter("data");
         }
-        
+               
         String idade;
-        if(!request.getParameter("idade").isEmpty()) {
-             idade = request.getParameter("idade");
+        if(request.getParameter("idade") == null) {
+            idade = "Idade não informada";
         } else {
-            idade = "Idade não enviada";
+            idade = request.getParameter("idade");
         }
         
         String descricao;
-        if(!request.getParameter("descricao").isEmpty()) {
-             descricao = request.getParameter("descricao");
+        if(request.getParameter("descricao").isEmpty()){
+            descricao = "Descrição não fornecida";
         } else {
-            descricao = "Descrição não enviada";
+            descricao = request.getParameter("descricao");
+        }
+                
+        
+        String turnos = "";
+        //Verificando o envio de determinado parâmetro
+        if(request.getParameterValues("turno[]") != null) {
+            String turno[] = request.getParameterValues("turno[]");
+            
+            //Varrendo array e lançando seus dados noutra String
+            for(int i=0; i < turno.length; i++){
+                turnos += turno[i] + "\n";
+            }
+        } else {
+            turnos = "Nenhum turno informado";
         }
         
-        String interesses = "";
-        String interesse[];
-        if(request.getParameterValues("interesse") != null) {
-             interesse = request.getParameterValues("interesse");
-             //Com a lista "interesse" preenchida, agora vamos varrê-la
-             //e jogar cada valor desta na String interesses.
-             for(int i=0; i < interesse.length; i++) {
-                 interesses += "- " + interesse[i] + "<br>";
-             }
-        } else {
-            interesses = "Nenhum interesse";
-        }
+        //Criando parâmetros para serem encaminhados à JSP
+        request.setAttribute("data", data);
+        request.setAttribute("idade", idade);
+        request.setAttribute("turnos", turnos);
+        request.setAttribute("descricao", descricao);
         
-        
-        
+        //Encaminhar parâmetros para a JSP
+        request.getRequestDispatcher("resultado.jsp").forward(request, response);
+                
         
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
@@ -60,12 +67,11 @@ public class Formulario extends HttpServlet {
             out.println("<html>");
             out.println("<head>");
             out.println("<title>Formulário</title>");            
-            out.println("<link rel='stylesheet' href='css/style.css'>");
             out.println("</head>");
             out.println("<body>");
             out.println("<h1>"+ data +"</h1>");
             out.println("<h1>"+ idade +"</h1>");
-            out.println("<h1>"+ interesses +"</h1>");
+            out.println("<h1>"+ turnos +"</h1>");
             out.println("<h1>"+ descricao +"</h1>");
             out.println("</body>");
             out.println("</html>");
@@ -89,7 +95,7 @@ public class Formulario extends HttpServlet {
     
     @Override
     public String getServletInfo() {
-        return "Validações de back-end";
+        return "Short description";
     }
 
 }
