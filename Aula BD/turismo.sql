3ad5cd364eb7787f603ccfc45fcde4f16e1e9216
teacherlittletooth <94143910+teacherlittletooth@users.noreply.github.com>
2023-04-25 14:37:59 -0300

    Create turismo.sql

---
+++ Aula BD/turismo.sql
@@ -0,0 +1,126 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3307
+-- Tempo de geração: 25-Abr-2023 às 19:35
+-- Versão do servidor: 10.4.27-MariaDB
+-- versão do PHP: 8.1.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `turismo`
+--
+CREATE DATABASE turismo;
+USE turismo;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `transportes`
+--
+
+CREATE TABLE `transportes` (
+  `cod_transporte` int(11) NOT NULL,
+  `tipo` varchar(20) NOT NULL,
+  `assentos` int(11) NOT NULL,
+  `combustivel` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `transportes_viagens`
+--
+
+CREATE TABLE `transportes_viagens` (
+  `cod_transporte_viagem` int(11) NOT NULL,
+  `cod_transporte` int(11) NOT NULL,
+  `cod_viagem` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `viagens`
+--
+
+CREATE TABLE `viagens` (
+  `cod_viagem` int(11) NOT NULL,
+  `local_partida` text NOT NULL,
+  `local_destino` text NOT NULL,
+  `data_viagem` date NOT NULL,
+  `hora_saida` time DEFAULT NULL,
+  `hora_chegada` time DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices para tabela `transportes`
+--
+ALTER TABLE `transportes`
+  ADD PRIMARY KEY (`cod_transporte`);
+
+--
+-- Índices para tabela `transportes_viagens`
+--
+ALTER TABLE `transportes_viagens`
+  ADD PRIMARY KEY (`cod_transporte_viagem`),
+  ADD KEY `cod_transporte` (`cod_transporte`),
+  ADD KEY `cod_viagem` (`cod_viagem`);
+
+--
+-- Índices para tabela `viagens`
+--
+ALTER TABLE `viagens`
+  ADD PRIMARY KEY (`cod_viagem`);
+
+--
+-- AUTO_INCREMENT de tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `transportes`
+--
+ALTER TABLE `transportes`
+  MODIFY `cod_transporte` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `transportes_viagens`
+--
+ALTER TABLE `transportes_viagens`
+  MODIFY `cod_transporte_viagem` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `viagens`
+--
+ALTER TABLE `viagens`
+  MODIFY `cod_viagem` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restrições para despejos de tabelas
+--
+
+--
+-- Limitadores para a tabela `transportes_viagens`
+--
+ALTER TABLE `transportes_viagens`
+  ADD CONSTRAINT `transportes_viagens_ibfk_1` FOREIGN KEY (`cod_transporte`) REFERENCES `transportes` (`cod_transporte`),
+  ADD CONSTRAINT `transportes_viagens_ibfk_2` FOREIGN KEY (`cod_viagem`) REFERENCES `viagens` (`cod_viagem`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
