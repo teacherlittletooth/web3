662b36869f626b7aef61e2aad20889d7f8dc0af4
teacherlittletooth <94143910+teacherlittletooth@users.noreply.github.com>
2023-04-25 15:50:36 -0300

    Update turismo.sql

--- Aula BD/turismo.sql
+++ Aula BD/turismo.sql
@@ -119,6 +119,37 @@ ALTER TABLE `viagens`
 ALTER TABLE `transportes_viagens`
   ADD CONSTRAINT `transportes_viagens_ibfk_1` FOREIGN KEY (`cod_transporte`) REFERENCES `transportes` (`cod_transporte`),
   ADD CONSTRAINT `transportes_viagens_ibfk_2` FOREIGN KEY (`cod_viagem`) REFERENCES `viagens` (`cod_viagem`);
+
+--
+-- Inserindo registros nas tabelas
+--
+INSERT INTO transportes(`assentos`, `combustivel`, `tipo`)
+VALUES  (45,'Diesel','Ônibus'),
+(30,'Flex',	'Van'),
+(100,'Gasolina', 'Avião'),
+(1,'Cavalo','Carroça'),
+(2,'Flex','Moto'),
+(5,'Flex','Carro');
+
+INSERT INTO `viagens`
+(`local_partida`, `local_destino`, `data_viagem`, `hora_saida`, `hora_chegada`)
+VALUES  ('Porto Alegre','Recife','2023-02-01','09:00:00','19:35:00'),
+('Porto Alegre','Rio de Janeiro','2023-01-05','10:00:00','15:00:00'),
+('Porto Alegre','Amazônia','2023-04-25','01:30:00','22:00:00'),
+('Porto Alegre','Viamão','2023-05-01','00:00:00','23:59:00');
+
+INSERT INTO `transportes_viagens`(`cod_transporte`, `cod_viagem`)
+VALUES (1,1),
+(2,1),
+(3,2),
+(4,2),
+(3,3),
+(4,3),
+(5,3),
+(1,4),
+(5,4),
+(6,4);
+
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
