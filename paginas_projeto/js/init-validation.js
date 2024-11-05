d363016f17d4bf46ee2817e161c8533c0ef1101c
teacherlittletooth <94143910+teacherlittletooth@users.noreply.github.com>
2024-11-05 20:18:36 -0300

    Create init-validation.js

---
+++ paginas_projeto/js/init-validation.js
@@ -0,0 +1,16 @@
+(() => {
+    'use strict';
+
+    const forms = document.querySelectorAll('.validar');
+
+    Array.from(forms).forEach(form => {
+        form.addEventListener('submit', event => {
+            if (!form.checkValidity()) {
+                event.preventDefault();
+                event.stopPropagation();
+            }
+
+            form.classList.add('was-validated');
+        }, false);
+    });
+})();
