9652e4ac8153a26612c2383298c779bd8a610e54
teacherlittletooth <94143910+teacherlittletooth@users.noreply.github.com>
2024-12-02 21:21:37 -0300

    Update bd.sql

--- paginas_projeto/bd.sql
+++ paginas_projeto/bd.sql
@@ -6,7 +6,7 @@ use sistema_java;
 
 -- Criando tabela de usuarios
 create table usuarios(
-	id int primary key auto_increment,
+    id int primary key auto_increment,
     nome varchar(100) not null,
     email varchar(100) not null unique,
     nascimento date not null,
@@ -18,3 +18,17 @@ create table usuarios(
 insert into usuarios(nome, email, nascimento, senha, noticias)
 values
 ( "admin", "admin@admin", "1970-01-01", sha1("ADM@123"), 0 );
+
+-- Criando a tabela de tarefas
+create table tarefas(
+    id int primary key auto_increment,
+    titulo varchar(50) not null,
+    descricao text,
+    status_tarefa varchar(20),
+    id_usuario int
+);
+
+-- Adicionando chave estrangeira ao atributo id_usuario
+-- da tabela de tarefas
+alter table tarefas add foreign key
+(id_usuario) references usuarios (id);
